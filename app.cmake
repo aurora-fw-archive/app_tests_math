d6ecf9b5ca0c07f19eb92002f4aa2dd913c98b6d
Ricardo Subtil <ricasubtil@gmail.com>
2017-08-28 13:49:52 +0100

    Update app.cmake

--- app.cmake
+++ app.cmake
@@ -4,7 +4,7 @@ if (NOT CONFIGURED_ONCE)
 	set(APP_TESTS_MATH_SOURCE_DIR ${APP_TESTS_MATH_DIR}/src)
 endif()
 
-#comment TODO - Replace test_string with upcoming test file
-#comment add_executable(aurorafw_app_tests_string ${APP_TESTS_MATH_SOURCE_DIR}/test_string.cpp)
+# TODO - Replace test_string with upcoming test file
+# add_executable(aurorafw_app_tests_string ${APP_TESTS_MATH_SOURCE_DIR}/test_string.cpp)
 
-#comment set_target_properties(aurorafw_app_tests_string PROPERTIES OUTPUT_NAME test_string.out)
+# set_target_properties(aurorafw_app_tests_string PROPERTIES OUTPUT_NAME test_string.out)
